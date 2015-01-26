4f1f2b8f272779a6a56dd373096a7ff588f4b3db
forsrc <forsrc@gmail.com>
2015-01-27 00:34:23 +0800

    CanyonBunny libgdx-1.5.3

---
+++ CanyonBunny/desktop/src/com/packtpub/libgdx/canyonbunny/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.packtpub.libgdx.canyonbunny.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.packtpub.libgdx.canyonbunny.CanyonBunnyMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CanyonBunnyMain(), config);
+	}
+}
---
+++ CanyonBunny/html/src/com/packtpub/libgdx/canyonbunny/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.packtpub.libgdx.canyonbunny.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.packtpub.libgdx.canyonbunny.CanyonBunnyMain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new CanyonBunnyMain();
+        }
+}
